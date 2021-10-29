d7d6fb63c952f3baf6bd92c69d5f9228dd1f4190
sj96715 <sj96715@Siddharth.com>
2021-10-29 18:58:59 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<head>
+  <!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+<style>
+    .container{
+ height:auto;
+ width:auto;
+ margin:20px auto;
+ 
+
+}
+ul{
+    font-size:30px;
+}
+ul li{
+    font-size: 15px;
+    ;
+}
+    </style>
+</head>
+
+<body>
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark primary-color" >
+
+  <!-- Navbar brand -->
+  <a class="navbar-brand" href="#" style="padding-left:20px">job categories</a>
+
+  <!-- Collapse button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#basicExampleNav"
+    aria-controls="basicExampleNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- Collapsible content -->
+  
+
+    <form class="form-inline">
+      <div class="md-form my-0" style="padding-left:900px;">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+      </div>
+    </form>
+  </div>
+  <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+<div class="container">
+<ul>technical & operational
+<li>it security</li>
+<li>programers</li>
+<li>web developes</li>
+<li>colputer system analyst</li>
+<li>network engineer</li></ul>
+<ul>professional
+<li>teachers</li>
+<li>doctors</li>
+<li>postal workers</li>
+<li>armed forces</li>
+</ul>
+<ul>supervisory and managerial
+<li>educator</li>
+<li>sponsor</li>
+<li>coach</li>
+<li>director</li>
+</ul>
+</ul>
+</div>
+</body>
